ce421ac7ebe3b802551c0372f97c3d8f3d8c07ac
AnishaSatapathy01@ <anishasatapathy23@gmail.com>
2025-05-21 22:37:02 +0530

    java practice

---
+++ DSA practice/GreaterTwo.java
@@ -0,0 +1,13 @@
+public class GreaterTwo {
+    public static void main (String args[]){
+       int a = 10;
+       int b = 151;
+       if (a>b){
+        System.out.println(a+"is greater");
+       }
+       else 
+       {
+        System.out.println(b+" is greater ");
+       }
+    }
+}
